3533221e11a89c49e244eed3f8b761f5bfab6e8e
Nema <shelly.nema@capgemini.com>
2024-05-14 11:39:56 +0530

    Top Tracks added

--- client/src/Pages/Route.jsx
+++ client/src/Pages/Route.jsx
@@ -1,28 +1,27 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Profile,TopArtists } from './index';
+import { createBrowserRouter } from "react-router-dom";
+import { Profile, TopArtists, TopTracks } from "./index";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Profile/>
-    },
-    {
-        path: '/top-artists',
-        element: <TopArtists/>
-    },
-    // {
-    //     path: '/top-tracks',
-    //     element: <Toptacks/>
-    // },
-    {
-        path: '/playlists/:id',
-        element: <Profile/>
-    },
-    {
-        path: '/playlists',
-        element: <Profile/>
-    }
-
-])
+  {
+    path: "/",
+    element: <Profile />,
+  },
+  {
+    path: "/top-artists",
+    element: <TopArtists />,
+  },
+  {
+    path: "/top-tracks",
+    element: <TopTracks />,
+  },
+  {
+    path: "/playlists/:id",
+    element: <Profile />,
+  },
+  {
+    path: "/playlists",
+    element: <Profile />,
+  },
+]);
 
 export default router;
---
+++ client/src/Pages/TopTracks.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { catchErrors } from "../utils";
+import { SectionWrapper, TimeRangeButtons, TrackList } from "../components";
+import { getTopTracks } from "../spotify";
+function TopTracks() {
+  const [topTracks, setTopTracks] = useState(null);
+  const [activeRange, setActiveRange] = useState("short");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userTopTracks = await getTopTracks(`${activeRange}_term`);
+      setTopTracks(userTopTracks.data);
+    };
+    catchErrors(fetchData());
+  }, [activeRange]);
+  return (
+    <>
+      {topTracks && (
+        <main>
+          <SectionWrapper title="Top Tracks" breadcrumb={true}>
+            <TimeRangeButtons
+              activeRange={activeRange}
+              setActiveRange={setActiveRange}
+            />
+            {topTracks && topTracks.items && (
+              <TrackList tracks={topTracks.items} />
+            )}
+          </SectionWrapper>
+        </main>
+      )}
+    </>
+  );
+}
+
+export default TopTracks;
